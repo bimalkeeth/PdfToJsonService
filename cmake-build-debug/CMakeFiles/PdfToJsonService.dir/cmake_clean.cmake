1f5125203cc2109c66ca16a9c3be692dfbfe9453
bimalkeeth@gmail.com <Scala@1234>
2019-09-08 20:36:23 +1000

    first

---
+++ cmake-build-debug/CMakeFiles/PdfToJsonService.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PdfToJsonService.dir/main.cpp.o"
+  "PdfToJsonService.pdb"
+  "PdfToJsonService"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PdfToJsonService.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/untitled8.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bill/CLionProjects/cexercise/Cexcecise/PdfToJsonService/main.cpp" "/home/bill/CLionProjects/cexercise/Cexcecise/PdfToJsonService/cmake-build-debug/CMakeFiles/untitled8.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPPREST_FORCE_HTTP_CLIENT_ASIO"
+  "CPPREST_FORCE_HTTP_LISTENER_ASIO"
+  "CPPREST_NO_SSL_LEAK_SUPPRESS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
